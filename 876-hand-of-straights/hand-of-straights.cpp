0e7d7c2bcbcb2e22dfe72cc21bd9c70dbddd7dcb
Raji Gupta <113358696+Rajiiee@users.noreply.github.com>
2024-06-06 16:24:16 +0530

    Time: 24 ms (99.76%) | Memory: 23.1 MB (99.39%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool findSuccessor(vector<int>& hand, int groupSize, int i){
+        int next=hand[i]+1;
+        hand[i]=-1;
+        int count=1;
+        i+=1;
+        while(i<hand.size() && count<groupSize){
+            if(hand[i]==next){
+                next=hand[i]+1;
+                hand[i]=-1;
+                count++;
+            }
+            i++;
+        }
+
+        return count==groupSize;
+
+    }
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if(hand.size() % groupSize != 0){
+            return false;
+        }
+
+        sort(hand.begin(),hand.end());
+        for(int i=0;i<hand.size();i++){
+            if(hand[i]>=0){
+                if(!findSuccessor(hand,groupSize,i)){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+    }
+};

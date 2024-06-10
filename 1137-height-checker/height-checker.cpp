94e1d462b72447d3de9fcc5ae6f434124a335858
Raji Gupta <113358696+Rajiiee@users.noreply.github.com>
2024-06-10 21:27:10 +0530

    Time: 5 ms (31.17%) | Memory: 10 MB (78.64%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -4,9 +4,8 @@ public:
         vector<int>sorted_heights=heights;
         sort(sorted_heights.begin(),sorted_heights.end());
 
-        int n=heights.size();
         int ans=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<heights.size();i++){
             if(heights[i]!=sorted_heights[i]){
                 ans++;
             }

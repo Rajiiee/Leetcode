3cdad0bd2c7139032f11e86b3cdabe9f19fefa87
Raji Gupta <113358696+Rajiiee@users.noreply.github.com>
2024-07-06 13:44:50 +0530

    Time: 3 ms (16.54%) | Memory: 7.3 MB (30.58%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int passThePillow(int n, int time) {
         int duration=time/(n-1);
-        int pos=time%(n-1);
         if(duration%2 == 0){
-            return pos+1;
+            return (time%(n-1))+1;
         }else{
-            return n-pos;
+            return n-(time%(n-1));
         }
     }
 };

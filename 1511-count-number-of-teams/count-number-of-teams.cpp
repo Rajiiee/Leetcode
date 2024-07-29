89273b22c008bd4dabf46c5ca27ba888afe542f5
Raji Gupta <113358696+Rajiiee@users.noreply.github.com>
2024-07-29 21:23:45 +0530

    Time: 26 ms (84.19%) | Memory: 11.7 MB (40.04%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -8,11 +8,11 @@ public:
 
             // Count ratings on the left of i
             for (int j = 0; j < i; j++) 
-                L[rating[j] <= rating[i]]++;//rating is unique
+                L[rating[j] < rating[i]]++;//rating is unique
             
             // Count ratings on the right of i
             for (int k=i+1; k<n; k++) 
-                R[rating[k] <= rating[i]]++;
+                R[rating[k] < rating[i]]++;
 
             // number of valid teams
             cnt += L[0]*R[1] + L[1]*R[0];
